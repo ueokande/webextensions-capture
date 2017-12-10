9a36b2a6e3588e7313e0d3ae7c74bda8af3d9301
Shin'ya Ueoka <ueokande@i-beam.org>
2017-12-10 21:48:40 +0900

    context menu and capture page

--- src/background/index.js
+++ src/background/index.js
@@ -1,1 +1,22 @@
-console.log('this is a background script');
+const MENU_CAPTURE = 'menu-capture';
+const MENU_CAPTURE_WHOLE_PAGE= 'menu-capture-whole-page';
+
+const captureWholePage = () => {
+  chrome.tabs.captureVisibleTab({ format: 'png' }, (url) => {
+    console.log('captured on ', url);
+  });
+};
+
+chrome.contextMenus.create({
+  id: MENU_CAPTURE,
+  title: 'Capture'
+});
+
+chrome.contextMenus.create({
+  id: MENU_CAPTURE_WHOLE_PAGE,
+  title: 'Capture whole page',
+  parentId: MENU_CAPTURE,
+  onclick: (info, tab) => {
+    captureWholePage();
+  }
+});
